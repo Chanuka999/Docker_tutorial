37a9fc0512f380408449c5991922efb3f7f373aa
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-25 13:23:55 +0530

    add database connection setup using mongoose

---
+++ docker_with_mernApplication/server/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDb = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("database connected");
+    })
+    .catch(() => {
+      console.log("database not connected");
+    });
+};
